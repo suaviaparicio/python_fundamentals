3b08a6d3d3bc734219832d8f972fc7d70a3ec149
laparici9 <laparici9@gmail.com>
2022-11-01 17:42:06 -0500

    se actualizan los ejercicios con correcciones. - Se sube el archivo de funcions_intermediate.

--- functions_basic_ii.py
+++ functions_basic_ii.py
@@ -1,38 +1,36 @@
 # 1. Cuenta regresiva: crea una función que acepte un número como entrada. Devuelve una lista nueva que cuente de uno en uno, desde el número (como elemento 0) hasta 0 (como último elemento). 
 #Ejemplo: countdown(5) debería devolver [5,4,3,2,1,0]
 
-# def count_down(num):
-#     lista = []
-#     while num >= 0:
-#         lista.append(num)
-#         num -= 1
-#     return lista
+def count_down(num):
+    lista = []
+    while num >= 0:
+        lista.append(num)
+        num -= 1
+    return lista
 
-# print(count_down(5))
+print(count_down(5))
 
 
 
 # 2. Imprimir y devolver: crea una función que reciba una lista con dos números. Imprime el primer valor y devuelve el segundo.
 #Ejemplo: imprimir_y_devolver([1,2]) debe imprimir 1 y devolver 2
 
-# def imprime_devuelve(lista):
-#     print(lista[0])
-#     devolver = lista[1]
-#     return devolver
+def imprime_devuelve(lista):
+    print(lista[0])
+    devolver = lista[1]
+    return devolver
 
-# imprime_devuelve([1,2])
-
-#--------------------------------------------------> PREGUNTAR A MIGUE SI ESTÁ BIEN, PORQUE FINALMENTE ME IMPRIME LOS DOS NÚMEROS <-------------------------------------------------------
+imprime_devuelve([1,2])
 
 
 # 3. Primero más longitud: crea una función que acepte una lista y devuelva la suma del primer valor de la lista, más la longitud de la lista.
 #Ejemplo: primero_mas_longitud([1,2,3,4,5]) debe devolver 6 (primer valor: 1 +length: 5)
 
-# def primero_mas_longitud(lista):
-#     sum = lista[0] + len(lista)
-#     return sum
+def primero_mas_longitud(lista):
+    sum = lista[0] + len(lista)
+    return sum
 
-# print(primero_mas_longitud([1,2,3,4,5]))
+print(primero_mas_longitud([1,2,3,4,5]))
 
 
 
@@ -41,19 +39,19 @@
 #Ejemplo: valores_mayores_que_el_segundo([5,2,3,2,1,4]) debe imprimir 3 y devolver [5,3,4]
 #Ejemplo: valores_mayores_que_el_segundo([3]) debe devolver False
 
-# def valores_mayores_que_el_segundo(lista):
-#     nueva_lista = []
-#     if len(lista) < 2:
-#         return False
+def valores_mayores_que_el_segundo(lista):
+    nueva_lista = []
+    if len(lista) < 2:
+        return False
 
-#     for x in lista:
-#         if x > lista[1]:
-#             nueva_lista.append(x)
-#     print(len(nueva_lista))
-#     return nueva_lista
+    for x in lista:
+        if x > lista[1]:
+            nueva_lista.append(x)
+    print(len(nueva_lista))
+    return nueva_lista
 
-# print(valores_mayores_que_el_segundo([5,2,3,2,1,4]))
-# print(valores_mayores_que_el_segundo([3]))
+print(valores_mayores_que_el_segundo([5,2,3,2,1,4]))
+print(valores_mayores_que_el_segundo([3]))
 
 
 
@@ -62,11 +60,11 @@
 #Ejemplo: length_and_value(4,7) debe devolver [7,7,7,7]
 #Ejemplo: length_and_value(6,2) debe devolver [2,2,2,2,2,2] 
 
-# def length_and_value(size, value):
-#     lista = []
-#     while len(lista) < size:
-#         lista.append(value)
-#     return lista
+def length_and_value(size, value):
+    lista = []
+    while len(lista) < size:
+        lista.append(value)
+    return lista
 
-# print(length_and_value(4,7))
-# print(length_and_value(6,2))
+print(length_and_value(4,7))
+print(length_and_value(6,2))
--- functions_intermediate.py
+++ functions_intermediate.py
@@ -1,4 +1,5 @@
 # 1. Actualizar valores en diccionarios y listas
+
 x = [[5, 2, 3], [10, 8, 9]]
 estudiantes = [
     {'first_name':  'Michael', 'last_name': 'Jordan'},
@@ -10,32 +11,98 @@ directorio_deportes = {
 }
 z = [{'x': 10, 'y': 20}]
 
-
 # 1.1. Cambia el valor 10 en x a 15. Una vez que hayas terminado, x ahora debería ser [ [5,2,3], [15,8,9] ].
 x[1][0] = 15
-print(x)
+# print(x)
 
 # 1.2. Cambia el "apellido” del primer alumno de 'Jordan' a 'Bryant'.
 estudiantes[0]['last_name'] = 'Bryant'
-print(estudiantes)
+# print(estudiantes)
 
 # 1.3. En el directorio_deportes, cambia "Messi" por "Andrés".
+directorio_deportes['fútbol'][0] = 'Andrés'
+# print(directorio_deportes)
+
 # 1.4. Cambia el valor 20 en z a 30.
+z[0]['y'] = 30
+# print(z)
 
 
 # 2. Iterar a través de una lista de diccionarios:
 # Crea una función iterateDictionary(some_list)para que, dada una lista de diccionarios, la función recorra cada diccionarios de la lista e imprima cada llave y el valor asociado.
-# Por ejemplo, dada la siguiente lista:
-# estudiantes = [
-#     {'first_name':  'Michael', 'last_name': 'Jordan'},
-#     {'first_name': 'John', 'last_name': 'Rosales'},
-#     {'first_name': 'Mark', 'last_name': 'Guillen'},
-#     {'first_name': 'KB', 'last_name': 'Tonel'}
-# ]
-# iterateDictionary(estudiantes)
-# debería devolver: (está bien si cada par clave-valor termina en 2 líneas separadas;
+
 # un bonus para que aparezcan exactamente como se muestra a continuación)
 # first_name - Michael, last_name - Jordan
 # first_name - John, last_name - Rosales
 # first_name - Mark, last_name - Guillen
 # first_name - KB, last_name - Tonel
+
+# Por ejemplo, dada la siguiente lista:
+
+estudiantes = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+def iterateDictionary(some_list):
+    for dict in some_list:
+        print(some_list)
+        
+
+
+def iterateDictionary(some_list):
+    for dict in some_list:
+        for key,value in dict.items():
+            print(f"{key} - {value},")
+
+x = iterateDictionary(estudiantes)
+print(x)
+
+
+# 3. Obtener valores de una lista de diccionarios:
+# Crea una función iterateDictionary2(key_name, some_list)que, dada una lista de diccionarios y un nombre de clave, la función imprima el valor almacenado en esa clave para cada diccionario.
+# Por ejemplo, iterateDictionary2('name', estudiantes) debería generar:
+
+#Michael
+#John
+#Mark
+#KB
+
+def iterateDictionary2(key_name, some_list):
+    for gato in some_list:
+        print(gato[key_name])
+
+estudiantes = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+x = iterateDictionary2('first_name', estudiantes)
+print(x)
+
+y = iterateDictionary2('last_name', estudiantes)
+print(y)
+
+
+# 4. Iterar a través de un diccionarios con valores de lista
+# Crea una función printInfo(some_dict)que, dado un diccionario cuyos valores son todos listas, imprima el nombre de cada clave junto con el tamaño de su lista,
+# y luego imprima los valores asociados dentro de la lista de cada clave. Por ejemplo:
+
+def printInfo(some_dict):
+    for keys, values in some_dict.items():
+        list_lenght = len(values)
+        print(f"{list_lenght} {keys.upper()}")
+
+        for value in values:
+            print(value)
+
+
+dojo = {
+    'ubicaciones': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructores': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
